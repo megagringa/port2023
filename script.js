b803f877e26dca2c51130e798ff9b40fc30f7a83
Juan Pablo Lopez <38148731+megagringa@users.noreply.github.com>
2023-04-18 23:08:31 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const typed = new Typed('.typed',{
+    strings:[
+        '<i>FullStack Developer</i>',
+        '<i>Backend Developer</i>',
+        '<i>Fronted Developer</i>'
+    ],
+    typeSpeed:75,
+    startDelay:300,
+    backSpeed:75,
+    loop:true
+});
