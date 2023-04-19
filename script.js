9ef3d163f7308aa843edfca676a9dfbb7765d604
Juan Pablo Lopez <38148731+megagringa@users.noreply.github.com>
2023-04-19 00:22:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const typed = new Typed('.typed',{
     strings:[
-        '<i>FullStack Developer</i>',
-        '<i>Backend Developer</i>',
-        '<i>Fronted Developer</i>'
+        '<i class="escritura">FullStack Developer</i>',
+        '<i class="escritura">Backend Developer</i>',
+        '<i class="escritura">Fronted Developer</i>'
     ],
     typeSpeed:75,
     startDelay:300,
